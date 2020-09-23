afb51b676dee43565d054d1b20c30d53fc5c9857
rupam999 <rupamchakraborty999@gmail.com>
2020-09-23 17:26:18 +0530

    Testing Ready

--- src/api/Config.ts
+++ src/api/Config.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 export const BASE_URL =
-    // "https://cors-anywhere.herokuapp.com/https://e-commerce-pwa-backend.herokuapp.com/";
-    "http://localhost:8000";
+    "https://cors-anywhere.herokuapp.com/https://e-commerce-pwa-backend.herokuapp.com/";
+// "http://localhost:8000";
 export const LOGIN = "/api/user/login";
 export const SEND_OTP = "/api/user/sendOTP";
 export const VERIFY_OTP = "/api/user/verifyPhoneNumber";
