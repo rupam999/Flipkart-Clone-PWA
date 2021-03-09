357b09147ff99438a419a6bf10c245b71e1859ed
rupam999 <rupamchakraborty999@gmail.com>
2021-03-09 11:40:57 +0530

    test 001

--- src/Screens/Desktop/Components/DesktopSlider.tsx
+++ src/Screens/Desktop/Components/DesktopSlider.tsx
@@ -7,7 +7,7 @@ const DesktopSlider = () => {
         <div style={{marginTop: 10, paddingLeft: 7.5, paddingRight: 7.5}}>
             <Carousel autoplay>
             <div className="slideContentSlide" >
-                <img src='https://lh3.googleusercontent.com/proxy/riAPgvOx3i_wQi2AUGXGDGATITpZKCW-N2sV21F035YC99OeU4SRNSV5RwjcmGYro4B2pmavO47yRci97RirPdsJbCIEy_dbfid-BRLNj9xU84-PDFP_r5rSS16YlNah-xgZ0LxMs11O1y3kqw' />
+                <img src='https://rukminim1.flixcart.com/flap/1000/1080/image/6cf5ca3509f98556.jpg' />
             </div>
             <div className="slideContentSlide" >
                 <p>jhkjh</p>
