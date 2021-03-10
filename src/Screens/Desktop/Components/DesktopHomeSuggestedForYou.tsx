6b150696b05ca587707330300d7b35c1bb765baf
rupam999 <rupamchakraborty999@gmail.com>
2021-03-10 10:51:16 +0530

    Basic Desktop Home Structure Added

---
+++ src/Screens/Desktop/Components/DesktopHomeSuggestedForYou.tsx
@@ -0,0 +1,61 @@
+import React from 'react';
+import { Row, Col } from 'antd';
+import './css/DesktopHomeSuggestedForYouStyle.css';
+
+const DesktopHomeSuggestedForYou = () => {
+    return(
+        <>
+        <div className="SuggestedProductHome">
+            <Row>
+                <Col span={24}>
+                    <div className="SuggestedProductHomeItem">
+                        All the products will display here
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        <div className="Home3banner">
+            <Row>
+                <Col span={8}>
+                    <div className="Home3bannerImage marginRight">
+                        <img alt="Wash5thFeb" 
+                            srcSet="https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50 2x, 
+                                https://rukminim1.flixcart.com/flap/480/480/image/1ce0c4c1fb501b45.jpg?q=50 1x" 
+                            src="https://rukminim1.flixcart.com/flap/480/480/image/1ce0c4c1fb501b45.jpg?q=50" 
+                        />
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <div className="Home3bannerImage marginRight">
+                        <img alt="Furniture Bestsellers" 
+                            srcSet="https://rukminim1.flixcart.com/flap/960/960/image/084789479074d2b2.jpg?q=50 2x, 
+                                https://rukminim1.flixcart.com/flap/480/480/image/084789479074d2b2.jpg?q=50 1x" 
+                            src="https://rukminim1.flixcart.com/flap/480/480/image/084789479074d2b2.jpg?q=50" 
+                        />
+                    </div>
+                </Col>
+                <Col span={8}>
+                    <div className="Home3bannerImage">
+                        <img alt="Wash5thFeb" 
+                            srcSet="https://rukminim1.flixcart.com/flap/960/960/image/1ce0c4c1fb501b45.jpg?q=50 2x, 
+                                https://rukminim1.flixcart.com/flap/480/480/image/1ce0c4c1fb501b45.jpg?q=50 1x" 
+                            src="https://rukminim1.flixcart.com/flap/480/480/image/1ce0c4c1fb501b45.jpg?q=50" 
+                        />
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        <div className="SuggestedProductHome">
+            <Row>
+                <Col span={24}>
+                    <div className="SuggestedProductHomeItem">
+                        All the products will display here
+                    </div>
+                </Col>
+            </Row>
+        </div>
+        </>
+    );
+}
+
+export default DesktopHomeSuggestedForYou;
--- src/Screens/Desktop/Content/DesktopHomeScreen.tsx
+++ src/Screens/Desktop/Content/DesktopHomeScreen.tsx
@@ -1,4 +1,6 @@
 import React, {useState} from 'react';
+import DesktopHomeDealsOfTheDay from '../Components/DesktopHomeDealsOfTheDay';
+import DesktopHomeSuggestedForYou from '../Components/DesktopHomeSuggestedForYou';
 import DesktopNavbar from '../Components/DesktopNavbar';
 import DesktopSlider from '../Components/DesktopSlider';
 import '../css/HomeScreenStyle.css';
@@ -9,6 +11,12 @@ export const DesktopHomeScreen = () => {
             <DesktopNavbar />
             <div style={{backgroundColor: '#F1F3F6'}}>
                 <DesktopSlider />
+                <DesktopHomeDealsOfTheDay />
+                <DesktopHomeSuggestedForYou />
+                <br />
+                <br />
+                <br />
+                <br />
             </div>
         </div>
     );
