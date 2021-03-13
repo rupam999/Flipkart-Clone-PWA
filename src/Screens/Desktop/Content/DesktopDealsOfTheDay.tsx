9a42a2693ed0039b940e5df5c3b1437dbbd91b0d
rupam999 <rupamchakraborty999@gmail.com>
2021-03-13 16:59:42 +0530

    Deals of the Day Structure Added

---
+++ src/Screens/Desktop/Content/DesktopDealsOfTheDay.tsx
@@ -0,0 +1,26 @@
+import React from 'react';
+import DesktopFooter from '../Components/DesktopFooter';
+import DesktopNavbar from '../Components/DesktopNavbar';
+import './css/DesktopDealsOfTheDayStyle.css';
+
+export const DesktopDealsOfTheDay = () => {
+    return(
+        <div>
+            <DesktopNavbar />
+                <div className="DealsOfTheDayContainer">
+                    <div className="DealsOfTheTitle">
+                        <h3>Deals of the Day</h3>
+                    </div>
+                    <div className="AllTheDealsOfTheDayProducts">
+                        <p>All the products</p>
+                        <p>All the products</p>
+                        <p>All the products</p>
+                        <p>All the products</p>
+                        <p>All the products</p>
+                        <p>All the products</p>
+                    </div>
+                </div>
+            <DesktopFooter />
+        </div>
+    );
+}
