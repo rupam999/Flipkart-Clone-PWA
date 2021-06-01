d506ed17efa30c3c70ccacecf13fe0772b347432
rupam999 <rupamchakraborty999@gmail.com>
2021-06-01 14:45:23 +0530

    Cart Basic Layout Added

---
+++ src/Screens/Desktop/Content/DesktopShoppingCart.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import DesktopFooter from '../Components/DesktopFooter';
+import DesktopNavbar from '../Components/DesktopNavbar';
+
+export const DesktopShoppingCart = () => {
+    return(
+        <div>
+            <DesktopNavbar />
+            <div>
+                <p>hjhjhk</p>
+            </div>
+            <DesktopFooter />
+        </div>
+    );
+}
