c0c351c11e7b8d1179f4cac40a483ddf96f87c7b
rupam999 <rupamchakraborty999@gmail.com>
2021-06-01 15:15:50 +0530

    Desktop Empty Cart Added

---
+++ src/Screens/Desktop/Components/DesktopEmptyCart.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import IMG from '../../../assets/empty_cart_desktop.png';
+import './css/DesktopEmptyShoppingCartStyle.css';
+
+const DesktopEmptyCart = (props) => {
+    const {heading, text} = props;
+    return (
+        <div className="desktopCartItem">
+            <div className="emptyDivContent">
+               <h3>My Cart</h3>
+                <div className="message">
+                    <div>
+                        <img src={IMG} alt="Empty Basket" />
+                        <h3>{heading}</h3>
+                        <p>{text}</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default DesktopEmptyCart; 
--- src/Screens/Desktop/Content/DesktopShoppingCart.tsx
+++ src/Screens/Desktop/Content/DesktopShoppingCart.tsx
@@ -1,13 +1,23 @@
-import React from 'react';
+import React, {useContext} from 'react';
+import { Store } from '../../../Context/Store';
+import DesktopEmptyCart from '../Components/DesktopEmptyCart';
 import DesktopFooter from '../Components/DesktopFooter';
 import DesktopNavbar from '../Components/DesktopNavbar';
 
 export const DesktopShoppingCart = () => {
+    const {user} = useContext(Store);
     return(
         <div>
             <DesktopNavbar />
             <div>
-                <p>hjhjhk</p>
+                {user && user.id ? (
+                    <p>hjkhk</p>
+                ) : (
+                    <DesktopEmptyCart 
+                        heading='Missing Cart items?'
+                        text='Login to see the items you added previously'
+                    />
+                )}
             </div>
             <DesktopFooter />
         </div>
