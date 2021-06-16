c9fe7bd24aea25ad125ea7503158b38605298d04
rupam999 <rupamchakraborty999@gmail.com>
2021-06-16 12:34:41 +0530

    Changed little UI

--- src/App.tsx
+++ src/App.tsx
@@ -1,49 +1,45 @@
-import React from "react";
-import "antd/dist/antd.css";
-import "antd-mobile/dist/antd-mobile.css";
-import { BrowserRouter as Router, Route } from "react-router-dom";
-import WindowDimensions from "./components/WindowDimensions";
-import { StoreProvider } from "./Context/Store";
+import React from 'react';
+import 'antd/dist/antd.css';
+import 'antd-mobile/dist/antd-mobile.css';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import WindowDimensions from './components/WindowDimensions';
+import { StoreProvider } from './Context/Store';
 import {
-  Login,
-  Register,
-  VerifyOTP,
-  UserInformationForm,
-} from "./Screens/Mobile/SignInFlow";
-import { MainNavigation } from "./Screens/Mobile/Navigation";
-import {
-  DesktopDetailProductPage,
-  DesktopHome,
-  DesktopSearchResult,
-} from "./Screens/Desktop/Content";
+	Login,
+	Register,
+	VerifyOTP,
+	UserInformationForm,
+} from './Screens/Mobile/SignInFlow';
+import { MainNavigation } from './Screens/Mobile/Navigation';
+import { DesktopHome } from './Screens/Desktop/Content';
 
 const App = () => {
-  const { width } = WindowDimensions();
-  if (width > 900) {
-    return (
-      <StoreProvider>
-        <Router>
-          <Route path="/" component={DesktopHome} />
-        </Router>
-      </StoreProvider>
-    );
-  } else {
-    {
-      /* Mobile Device Screen */
-    }
-    return (
-      <StoreProvider>
-        <Router>
-          <Route exact path="/" component={Login} />
-          <Route exact path="/login" component={Login} />
-          <Route exact path="/register" component={Register} />
-          <Route exact path="/verifyOTP" component={VerifyOTP} />
-          <Route exact path="/information" component={UserInformationForm} />
-          <Route path="/user" component={MainNavigation} />
-        </Router>
-      </StoreProvider>
-    );
-  }
+	const { width } = WindowDimensions();
+	if (width > 900) {
+		return (
+			<StoreProvider>
+				<Router>
+					<Route path="/" component={DesktopHome} />
+				</Router>
+			</StoreProvider>
+		);
+	} else {
+		{
+			/* Mobile Device Screen */
+		}
+		return (
+			<StoreProvider>
+				<Router>
+					<Route exact path="/" component={Login} />
+					<Route exact path="/login" component={Login} />
+					<Route exact path="/register" component={Register} />
+					<Route exact path="/verifyOTP" component={VerifyOTP} />
+					<Route exact path="/information" component={UserInformationForm} />
+					<Route path="/user" component={MainNavigation} />
+				</Router>
+			</StoreProvider>
+		);
+	}
 };
 
 export default App;
--- src/Screens/Desktop/Components/DesktopPaymentMode.tsx
+++ src/Screens/Desktop/Components/DesktopPaymentMode.tsx
@@ -1,74 +1,96 @@
-import React from "react";
-import { Radio, Button, Space } from "antd";
-import { CheckCircleOutlined } from "@ant-design/icons";
-import { getPaymentDetails } from "../../../api/getPaymentDetails";
-import "./css/DesktopPaymentMode.css";
+import React, { useState } from 'react';
+import { Radio, Button, Space, Modal } from 'antd';
+import { CheckCircleOutlined } from '@ant-design/icons';
+import { getPaymentDetails } from '../../../api/getPaymentDetails';
+import './css/DesktopPaymentMode.css';
 
 const DesktopPaymentMode = (props) => {
-  const onChange = (e) => {
-    console.log("radio checked", e.target.value);
-  };
+	const [paymentMethod, setPaymentMethod] = useState<Number>();
 
-  const payNow = async () => {
-    const data = await getPaymentDetails();
-    if (data !== -1) {
-      const options = {
-        key: "rzp_test_p6rOeQwQ4iyQ8x",
-        amount: data.amount.toString(),
-        currency: data.currency,
-        name: "Ecommerce",
-        description: "Description",
-        // "image": IMAGE,
-        order_id: data.id,
-        handler: async function (response) {
-          console.log(response);
-          // alert(response.razorpay_payment_id),
-          // alert(response.razorpay_subscription_id),
-          // alert(response.razorpay_signature);
-        },
-        prefill: {
-          name: "hjkhkj",
-          email: "abc@example.com",
-        },
-        theme: {
-          color: "#F37254",
-        },
-      };
+	const onChange = (e) => {
+		setPaymentMethod(Number(e.target.value));
+	};
 
-      const _window = window as any;
-      const paymentObject = new _window.Razorpay(options);
-      paymentObject.open();
-    }
-  };
+	const payLatter = () => {
+		console.log('Pay Latter');
+	};
 
-  return (
-    <div className="desktopPaymentSection">
-      <div className="deliveryDisabled">
-        <h3>Delivery Address</h3>
-        <h3>
-          Done <CheckCircleOutlined />
-        </h3>
-      </div>
-      <div className="choiceOfPayment">
-        <div className="heading">
-          <h3>Payment Options</h3>
-        </div>
-        <div className="background">
-          <div className="paymentChoice">
-            <Radio.Group onChange={onChange} value={1} className="">
-              <Space direction="vertical">
-                <Radio value="1">Cash And Delivery</Radio>
-                <Radio value="5">Online Payment</Radio>
-              </Space>
-            </Radio.Group>
-          </div>
-          <div className="placeButton">
-            <Button onClick={payNow}>Continue and Place Order</Button>
-          </div>
-        </div>
-      </div>
-    </div>
-  );
+	const payNow = async () => {
+		try {
+			const data = await getPaymentDetails();
+			if (data !== -1) {
+				const options = {
+					key: 'rzp_test_p6rOeQwQ4iyQ8x',
+					amount: data.amount.toString(),
+					currency: data.currency,
+					name: 'Ecommerce',
+					description: 'Description',
+					// "image": IMAGE,
+					order_id: data.id,
+					handler: async function (response) {
+						console.log(response);
+						// alert(response.razorpay_payment_id),
+						// alert(response.razorpay_subscription_id),
+						// alert(response.razorpay_signature);
+					},
+					prefill: {
+						name: 'hjkhkj',
+						email: 'abc@example.com',
+					},
+					theme: {
+						color: '#F37254',
+					},
+				};
+
+				const _window = window as any;
+				const paymentObject = new _window.Razorpay(options);
+				paymentObject.open();
+			}
+		} catch (error) {
+			console.log('Error @ Payment', error);
+			Modal.error({
+				title: 'Error',
+				content: 'Some Error occurred! Please try again after sometime...',
+			});
+		}
+	};
+
+	const st = {};
+
+	return (
+		<div className="desktopPaymentSection">
+			<div className="deliveryDisabled">
+				<h3>Delivery Address</h3>
+				<h3>
+					Done <CheckCircleOutlined />
+				</h3>
+			</div>
+			<div className="choiceOfPayment">
+				<div className="heading">
+					<h3>Payment Options</h3>
+				</div>
+				<div className="background">
+					<div className="paymentChoice">
+						<Radio.Group onChange={onChange} value={1} style={st}>
+							<Space direction="vertical">
+								<Radio value="1">Cash And Delivery</Radio>
+								<Radio value="2">Online Payment</Radio>
+							</Space>
+						</Radio.Group>
+					</div>
+					<div className="placeButton">
+						{paymentMethod === 2 ? (
+							<Button onClick={payNow}>Continue and Pay Now</Button>
+						) : paymentMethod === 1 ? (
+							<Button onClick={payLatter}>
+								Continue with Cash and Delivery
+							</Button>
+						) : null}
+					</div>
+				</div>
+			</div>
+		</div>
+	);
 };
 
 export default DesktopPaymentMode;
